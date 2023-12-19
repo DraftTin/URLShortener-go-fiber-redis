77d0fd7c36fbab4f902a16738a2768b99372f191
flyingbutton <thedrafttin@gmail.com>
2023-12-19 20:58:47 +0800

    update

--- api/routes/resolve.go
+++ api/routes/resolve.go
@@ -16,6 +16,11 @@ func ResolveURL(c *fiber.Ctx) error {
 	value, err := r.Get(database.Ctx, url).Result()
 	if err == redis.Nil {
 		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": "short not found in the database"})
+	} else if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "cannot connect to the databse"})
 	}
+
+	rInr := database.CreateClient(1)
+	defer rInr.Close()
 
 }
